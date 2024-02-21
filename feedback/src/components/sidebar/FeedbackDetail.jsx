e84e006d1384aa749b871dfdddbe830fc152ac58
lucymutunga <lucyalphonce18@gmail.com>
2024-02-21 09:42:36 +0300

    adding comment details mobile design

--- feedback/src/components/sidebar/FeedbackDetail.jsx
+++ feedback/src/components/sidebar/FeedbackDetail.jsx
@@ -131,17 +131,19 @@ const FeedbackDetail = () => {
         </div>
       </div>
       <div className="comment-container mt-8  start-0 ml-4 w-80 h-50 bg-white">
-        <h1 className="font-semibold  py-8 px-4">Add Comment</h1>
+        <h1 className="font-semibold  py-6 px-4">Add Comment</h1>
 
         <input
           type="text"
           placeholder="Type your comment here"
-          className="w-[279] h-[80] bg-[#3A4374]  py-4 px-4 ml-4"
+          className="w-11/12  h-[80] bg-[#F7F8FD]  py-4 px-4 ml-4 "
         />
 
-        <div className="flex ml-4 mt-2">
-          <p>250 characters left</p>
-          <button className="bg-[#AD1FEA]">Post Comment</button>
+        <div className="flex  justify-between ml-4 py-8">
+          <p className="text-[#647196]">250 characters left</p>
+          <button className="bg-[#AD1FEA] rounded-lg text-white p-1 mr-2">
+            Post Comment
+          </button>
         </div>
       </div>
     </div>
