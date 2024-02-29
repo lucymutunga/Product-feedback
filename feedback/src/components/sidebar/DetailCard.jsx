db5b75ac9ba598824984839583f3af47de12b0fc
lucymutunga <lucyalphonce18@gmail.com>
2024-02-29 22:27:50 +0300

    finishing on details md and lg card components

--- feedback/src/components/sidebar/DetailCard.jsx
+++ feedback/src/components/sidebar/DetailCard.jsx
@@ -1,10 +1,30 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import data from "../../../data.json";
+
 const DetailCard = () => {
+  const { suggestionId } = useParams();
+  const [suggestions, setSuggestions] = useState([]);
+  const [selectedSuggestion, setSelectedSuggestion] = useState(null);
+
+  useEffect(() => {
+    setSuggestions(data.productRequests);
+    const parsedSuggestionId = parseInt(suggestionId, 10);
+    setSelectedSuggestion(parsedSuggestionId);
+  }, [suggestionId]);
+
+  const selectedSuggestionData = selectedSuggestion
+    ? suggestions.find((suggestion) => suggestion.id === selectedSuggestion)
+    : null;
+
+  const selectedSuggestionComments = selectedSuggestionData
+    ? selectedSuggestionData.comments
+    : [];
+
   return (
     <div className="container bg-[#F7F8FD] w-96 h-max p-4">
       <div className="flex justify-between p-6">
-        <div className="flex p-1 ">
+        <div className="flex p-1">
           <svg
             className="mt-2 mr-1"
             xmlns="http://www.w3.org/2000/svg"
@@ -25,48 +45,102 @@ const DetailCard = () => {
           </button>
         </Link>
       </div>
-      <div className="rounded-lg border-8 border-white bg-[#FFFFFF]">
-        <div className="flex space-x-4">
-          <span className="flex flex-col rounded-md bg-[#F7F8FD] w-8 h-12 p-2 ">
-            <svg
-              className="ml-1"
-              xmlns="http://www.w3.org/2000/svg"
-              width="9"
-              height="7"
-              viewBox="0 0 9 7"
-              fill="none"
-            >
-              <path d="M0 6L4 2L8 6" stroke="#4661E6" stroke-width="2" />
-            </svg>
-            <p>99</p>
-          </span>
-          <div className="flex flex-col">
-            <h2 className="font-bold text-[#3A4374]">
-              Add a dark theme option
-            </h2>
-            <p className="text-[#647196] font-light">
-              It would help people like me with light sensitivities.
-            </p>
-            <h2 className="text-[#4661E6] text-center  rounded-lg bg-[#F2F4FF] w-28 h-7 mt-2">
-              Feature
-            </h2>
+      {selectedSuggestionData && (
+        <div className="rounded-lg border-8 border-white bg-[#FFFFFF]">
+          <div className="flex space-x-4">
+            <span className="flex flex-col rounded-md bg-[#F7F8FD] w-8 h-12 p-2 ">
+              <svg
+                className="ml-1"
+                xmlns="http://www.w3.org/2000/svg"
+                width="9"
+                height="7"
+                viewBox="0 0 9 7"
+                fill="none"
+              >
+                <path d="M0 6L4 2L8 6" stroke="#4661E6" strokeWidth="2" />
+              </svg>
+              <p>{selectedSuggestionData.upvotes}</p>
+            </span>
+            <div className="flex flex-col">
+              <h2 className="font-bold text-[#3A4374]">
+                {selectedSuggestionData.title}
+              </h2>
+              <p className="text-[#647196] font-light">
+                {selectedSuggestionData.description}
+              </p>
+              <h2 className="text-[#4661E6] text-center  rounded-lg bg-[#F2F4FF] w-28 h-7 mt-2">
+                {selectedSuggestionData.category}
+              </h2>
+            </div>
+            <span className="flex mt-2">
+              <svg
+                className="mt-1"
+                xmlns="http://www.w3.org/2000/svg"
+                width="18"
+                height="16"
+                viewBox="0 0 18 16"
+                fill="none"
+              >
+                <path
+                  d="M2.62074 16H1.34534L2.24718 15.0895C2.73344 14.5986 3.0371 13.9601 3.11873 13.2674C1.03637 11.8878 0 9.88917 0 7.79388C0 3.92832 3.51913 0 9.0305 0C14.8692 0 18 3.61479 18 7.45522C18 11.321 14.8361 14.9333 9.0305 14.9333C8.0135 14.9333 6.95226 14.7963 6.00478 14.5448C5.10787 15.4735 3.89262 16 2.62074 16Z"
+                  fill="#CDD2EE"
+                />
+              </svg>
+              <p className="ml-1">{selectedSuggestionComments.length}</p>
+            </span>
           </div>
-          <span className="flex mt-2">
-            <svg
-              className="mt-1"
-              xmlns="http://www.w3.org/2000/svg"
-              width="18"
-              height="16"
-              viewBox="0 0 18 16"
-              fill="none"
-            >
-              <path
-                d="M2.62074 16H1.34534L2.24718 15.0895C2.73344 14.5986 3.0371 13.9601 3.11873 13.2674C1.03637 11.8878 0 9.88917 0 7.79388C0 3.92832 3.51913 0 9.0305 0C14.8692 0 18 3.61479 18 7.45522C18 11.321 14.8361 14.9333 9.0305 14.9333C8.0135 14.9333 6.95226 14.7963 6.00478 14.5448C5.10787 15.4735 3.89262 16 2.62074 16Z"
-                fill="#CDD2EE"
-              />
-            </svg>
-            <p className="ml-1">4</p>
-          </span>
+        </div>
+      )}
+
+      <div className="comments-container bg-[#FFF] w-80 ml-3">
+        <h1 className="text-[#3A4374] font-bold ml-5 p-4">
+          {selectedSuggestionComments.length} Comments
+        </h1>
+
+        {selectedSuggestion !== null &&
+        selectedSuggestionComments.length > 0 ? (
+          <div className="selected-suggestion-comments">
+            {selectedSuggestionComments.map((comment) => (
+              <div key={comment.id} className="border-b mt-2">
+                <div className="flex justify-between p-6">
+                  <img
+                    src={comment.user.image}
+                    alt="user"
+                    className="w-10 h-10"
+                  />
+                  <div className="flex flex-col">
+                    <p className="text-[#3A4374] font-semibold">
+                      {comment.user.name}
+                    </p>
+                    <p className="text-[#647196] ">{comment.user.username}</p>
+                  </div>
+                  <button className="text-[#4661E6] font-semibold">
+                    Reply
+                  </button>
+                </div>
+                <p className="p-4">{comment.content}</p>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <p>No comments yet.</p>
+        )}
+      </div>
+
+      <div className="comment-container mt-8 start-0 ml-4 w-80 h-50 bg-white">
+        <h1 className="font-semibold  py-6 px-4">Add Comment</h1>
+
+        <input
+          type="text"
+          placeholder="Type your comment here"
+          className="w-11/12  h-[80] bg-[#F7F8FD]  py-4 px-4 ml-4 "
+        />
+
+        <div className="flex  justify-between ml-4 py-8">
+          <p className="text-[#647196]">250 characters left</p>
+          <button className="bg-[#AD1FEA] rounded-lg text-white p-1 mr-2">
+            Post Comment
+          </button>
         </div>
       </div>
     </div>
